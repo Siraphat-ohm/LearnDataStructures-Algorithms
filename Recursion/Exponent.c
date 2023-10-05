ee0086544eef2108a590ae22e760df43ff422147
Siraphat-ohm <siraphat975@gmail.com>
2023-10-05 14:25:03 +0700

    more efficient exponent function

--- Recursion/Exponent.c
+++ Recursion/Exponent.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 
-int power( int m, int n ){
+int power1( int m, int n ){
     if ( n == 0 )
         return 1;
-    return power(m, n - 1 ) * m;
+    return power1(m, n - 1 ) * m;
+}
+
+int power2(int m, int n){
+    if ( n == 0 )
+        return 1;
+    if ( n%2 == 0 ) 
+        return power2(m*m, n/2);
+    else
+        return m * power2(m*m, (n-1)/2 );
+}
+
+int Ipower(int m, int n){
+    if ( n % 2 == 0 ){
+        int r = 1;
+        for ( int i = 0 ; i < n/2; i++){
+            r *= m * m;
+        }
+        return r;
+    } else {
+        int r = m;
+        for ( int i = 0; i < (n-1)/2; i++){
+            r *= m*m;
+        }
+        return r;
+    }
 }
 
 int main(){
     int r;
-    r = power(2, 3) ;
+    r = Ipower(2, 4) ;
     printf("%d", r);
     return 0;
 }
