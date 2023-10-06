4e9bb960a36333c056c7ffccb42e4195e19bf800
Siraphat-ohm <siraphat975@gmail.com>
2023-10-06 12:53:45 +0700

    Fibonacci

---
+++ Recursion/Fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int fib( int n ){
+    if ( n <= 1 )
+        return n;
+    return fib(n-2) + fib(n-1);
+}
+
+int f[10];
+int fib1( int n ){
+    if ( n <= 1 ){
+        f[n] = n;
+        return n;
+    } else {
+        if ( f[n - 2] == -1 ){
+            f[n-2] = fib1(n-2);
+        }
+        if ( f[n - 1] == -1){
+            f[n-1] = fib1(n-1);
+        }
+        return f[n-2] + f[n-1];
+    }
+}
+
+int main(){
+    for ( int i = 0; i < 10 ; i++ ) f[i] = -1;
+    printf("%d", fib1(7));
+    return 0;
+}
